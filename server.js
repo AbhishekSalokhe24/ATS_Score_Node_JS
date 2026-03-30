de942e20e1a209a9c0403e7e5591684131483a03
abhishek-GMS <abhisaloke2424@gmail.com>
2026-03-30 07:00:50 +0530

    in server.js CROS error solved

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 // ── Middleware ─────────────────────────────────────────────────────────
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://resume-ats-abhishek.netlify.app/"],
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
